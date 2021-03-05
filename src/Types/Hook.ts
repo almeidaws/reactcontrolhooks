da8b9c948579e4795ce1c2184871d9ee63ae9492
Gustavo Almeida <almeidaws@outlook.com>
2021-03-05 13:46:38 -0300

    Change alias Hook

--- src/Types/Hook.ts
+++ src/Types/Hook.ts
@@ -1,4 +1,6 @@
 import Neutralizable from './Neutralizable';
 import HookParams from './HookParams';
-type Hook<P extends HookParams, R> = (args?: Neutralizable<P>) => R;
+type Hook<P extends HookParams, R> =
+  | ((args?: Neutralizable<P>) => R)
+  | ((args: Neutralizable<P>) => R);
 export default Hook;
