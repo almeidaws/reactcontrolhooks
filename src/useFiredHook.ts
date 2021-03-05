5d0da41f8fe49f6585b400539cd70bd20dc83f24
Gustavo Almeida <almeidaws@outlook.com>
2021-03-05 12:17:29 -0300

    Remove redudant argument passing at useFiredHook

--- src/useFiredHook.ts
+++ src/useFiredHook.ts
@@ -2,17 +2,15 @@ import { useState } from 'react';
 import { Hook, HookParams, Neutralizable } from './Types';
 
 function useFiredHook<P extends HookParams, R>(
-  useDeferredHook: Hook<P, R>,
-  outerArgs?: Neutralizable<P>
-): [(innerArgs?: Neutralizable<P>) => void, R] {
+  useDeferredHook: Hook<P, R>
+): [(args: Neutralizable<P>) => void, R] {
   const [previousArgs, setPreviousArgs] = useState<
     Neutralizable<P> | undefined
   >(null);
   const result = useDeferredHook.apply(null, [previousArgs]);
 
-  const fire = (innerArgs?: Neutralizable<P>) => {
+  const fire = (innerArgs: Neutralizable<P>) => {
     if (innerArgs !== undefined) setPreviousArgs(innerArgs);
-    else setPreviousArgs(outerArgs);
   };
 
   return [fire, result];
