c719bf3b5cbf0c57c5b22473b0c4941265d9e771
Gustavo Almeida <almeidaws@outlook.com>
2021-04-01 15:49:20 -0300

    Change signature of useFiredHook

--- src/Types/Hook.ts
+++ src/Types/Hook.ts
@@ -1,6 +1,4 @@
 import Neutralizable from './Neutralizable';
 import HookParams from './HookParams';
-type Hook<P extends HookParams, R> =
-  | ((args?: Neutralizable<P>) => R)
-  | ((args: Neutralizable<P>) => R);
+type Hook<P extends HookParams, R> = (args?: Neutralizable<P>) => R;
 export default Hook;
--- src/useFiredHook.ts
+++ src/useFiredHook.ts
@@ -3,14 +3,14 @@ import { Hook, HookParams, Neutralizable } from './Types';
 
 function useFiredHook<P extends HookParams, R>(
   useDeferredHook: Hook<P, R>
-): [(args: Neutralizable<P>) => void, R] {
+): [(args?: Neutralizable<P>) => void, R] {
   const [previousArgs, setPreviousArgs] = useState<
     Neutralizable<P> | undefined
   >(null);
   const result = useDeferredHook.apply(null, [previousArgs]);
 
-  const fire = (innerArgs: Neutralizable<P>) => {
-    if (innerArgs !== undefined) setPreviousArgs(innerArgs);
+  const fire = (innerArgs?: Neutralizable<P>) => {
+    setPreviousArgs(innerArgs);
   };
 
   return [fire, result];
