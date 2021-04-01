9b8ae1fbe685d796334fcdea665d1b4f4af0838d
Gustavo Almeida <almeidaws@outlook.com>
2021-04-01 17:38:32 -0300

    Fix type error

--- src/useFiredIterate.ts
+++ src/useFiredIterate.ts
@@ -4,16 +4,15 @@ import { FallibleHook, HookParams, Neutralizable } from './Types';
 
 const useFiredIterate = <P extends HookParams, R, E extends Error>(
   hook: FallibleHook<P, R, E>,
-  args: Neutralizable<P>[],
   buffer: number
 ): [
-  (args: Neutralizable<Neutralizable<P>[]>) => void,
+  (args?: Neutralizable<Neutralizable<P>[]>) => void,
   [R[] | null, E[] | null]
 ] => {
   const useIterateOverHook = (args?: Neutralizable<Neutralizable<P>[]>) => {
     return useIterate(hook, args, buffer);
   };
-  return useFiredHook(useIterateOverHook, args);
+  return useFiredHook(useIterateOverHook);
 };
 
 export default useFiredIterate;
